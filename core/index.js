2465102be9a66cc6df889e2673e209618f9d5d3e
amr33mohamd <amr2010mohamd2010@gmail.com>
2017-10-11 23:42:23 +0200

    aaaa

--- core/index.js
+++ core/index.js
@@ -9,6 +9,7 @@
   app.set('view engine', 'ejs');
   app.use(express.static('public'));
 
+  
   con = mysql.createConnection({
   host: "etdq12exrvdjisg6.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
   user: "owkt5vetcd0d75ux",
@@ -16,8 +17,16 @@
   database:"phe73ypvqplst1jz",
   charset: 'utf8'
   });
-
-
+  
+  /*
+   con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database:"shopping1",
+  charset: 'utf8'
+  });
+*/
 var sql1 = "SET CHARACTER SET utf8";
  con.query(sql1, function (err, result) {
  var sql = "SET SESSION collation_connection ='utf8_general_ci";
